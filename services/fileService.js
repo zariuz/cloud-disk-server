4b5af6565e907724a764f5eb86182106d860d72d
zariuz <nechetov@hotmail.com>
2021-05-01 01:44:37 +0300

    edit path

--- services/fileService.js
+++ services/fileService.js
@@ -5,6 +5,7 @@ const config = require('config');
 class FileService {
   createDir(file) {
     const filePath = `${config.get('filePath')}\\${file.user}\\${file.path}`;
+    // const filePath = path.join(__dirname, `./../files/${file.user}/${file.path}`);
     return new Promise((resolve, reject) => {
       try {
         if (!fs.existsSync(filePath)) {
@@ -14,6 +15,7 @@ class FileService {
           return reject({message: 'File already exist'});
         }
       } catch (e) {
+        console.log(e);
         return reject({message: 'File error'});
       }
     });
