89a4d157f16ffb054194644e87bfa75a764fcced
zariuz <nechetov@hotmail.com>
2021-05-08 19:18:29 +0300

    add upload and del avatar

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,7 @@
 const fileService = require('../services/fileService');
 const config = require('config');
 const fs = require('fs');
+const Uuid = require('uuid');
 const User = require('../models/User');
 const File = require('../models/File');
 
@@ -110,7 +111,8 @@ class FileController {
   async downloadFile(req, res) {
     try {
       const file = await File.findOne({_id: req.query.id, user: req.user.id});
-      const path = config.get('filePath') + '\\' + req.user.id + '\\' + file.path;
+      // const path = config.get('filePath') + '\\' + req.user.id + '\\' + file.path;
+      const path = fileService.getPath(file);
       if (fs.existsSync(path)) {
         return res.download(path, file.name);
       }
@@ -145,6 +147,34 @@ class FileController {
     } catch (e) {
       console.log(e);
       return res.status(400).json({message: 'Search error'});
+    }
+  }
+
+  async uploadAvatar(req, res) {
+    try {
+      const file = req.files.file;
+      const user = await User.findById(req.user.id);
+      const avatarName = Uuid.v4() + '.jpg';
+      file.mv(config.get('staticPath') + '\\' + avatarName);
+      user.avatar = avatarName;
+      await user.save();
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({message: 'Upload avatar error'});
+    }
+  }
+
+  async deleteAvatar(req, res) {
+    try {
+      const user = await User.findById(req.user.id);
+      fs.unlinkSync(config.get('staticPath') + '\\' + user.avatar);
+      user.avatar = null;
+      await user.save();
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({message: 'Delete avatar error'});
     }
   }
 }
